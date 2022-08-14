1dc535529c2cce7a8bc0b4ffe4b6e967692bb1e6
LaLacou <108777796+users.noreply.github.com>
2022-08-14 12:37:56 -0700

    added some styling and created function for adding tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 var appt = {};
 
-var createAppt = function
+var createAppt = function(taskList) {
+    //create elements that will make up the appt slot
+    var apptLi = $('<li>').addClass('list-group-item');
+    var apptP = $('<p>').addClass('m-1').text(apptText);
+
+    apptLi.append(apptP);
+
+    $("#list-" + taskList).append(apptLi);
+}
+
+//appt time was clicked
+$('.list-group').on('click', function() {
+    
+    var textInput = $('<textarea>').addClass('form-control').val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger('focus');
+});
+
+('.list-group').on('blur', 'textarea', function(){
+    var text = $(this).val();
+
+    
+})
