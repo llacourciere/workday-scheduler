0d5ee1cae45d911a6137da43c32f5dc97dfebfd3
LaLacou <108777796+users.noreply.github.com>
2022-08-14 13:22:32 -0700

    added text area editor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,37 @@
 var appt = {};
 
-var createAppt = function(taskList) {
-    //create elements that will make up the appt slot
-    var apptLi = $('<li>').addClass('list-group-item');
-    var apptP = $('<p>').addClass('m-1').text(apptText);
+// var createAppt = function(taskList) {
+//     //create elements that will make up the appt slot
+//     var apptLi = $('<li>').addClass('list-group-item');
+//     var apptP = $('<p>').addClass('m-1').text(apptText);
 
-    apptLi.append(apptP);
+//     apptLi.append(apptP);
 
-    $("#list-" + taskList).append(apptLi);
-}
+//     $("#list-" + taskList).append(apptLi);
+// }
 
 //appt time was clicked
-$('.list-group').on('click', function() {
-    
+$('.list-group').on('click', function () {
+
+    var text = $(this)
+        .text()
+        .trim();
     var textInput = $('<textarea>').addClass('form-control').val(text);
     $(this).replaceWith(textInput);
     textInput.trigger('focus');
 });
 
-('.list-group').on('blur', 'textarea', function(){
+('.list-group').on('blur', 'textarea', function () {
+
+    console.log('blur');
     var text = $(this).val();
 
-    
-})
+    //recreate p element
+    var apptP = $('<p>')
+        .addClass('m-1')
+        .text(text);
+    //replacce text area with p element
+    $(this).replaceWith(apptP);
+});
+
+//save button clicked
