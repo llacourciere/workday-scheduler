ce18aed276e4ba54552d5087f57826389a9b4bdb
LaLacou <108777796+users.noreply.github.com>
2022-09-05 15:12:06 -0700

    createad dynamic time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-var appt = {};
 
+let store;
+
+const loadStorage = async ()=>{
+    if(localStorage.hours){
+        let store = await eval(localStorage.hours);
+        
+        store.forEach((ms, i) => {
+            $('textarea').eq(i).val(ms);
+        });
+    }
+};
+
+loadStorage();
 //show date and time at top of page
 
 var date = document.querySelector('#currentDay')
@@ -7,44 +19,33 @@ var currentDay = moment();
 
 date.textContent = currentDay.format('MMMM Do YYYY, h:mm a')
 
-var createAppt = function (apptText, apptList) {
-    //create elements that will make up the appt slot
-    var apptLi = $('<li>').addClass('list-group-item');
-    var apptP = $('<p>').addClass('m-1').text(apptText);
+const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+let cHour = moment().format('H');
 
-    apptLi.append(apptP);
-
-    $("#list-" + apptList).append(apptLi);
-}
-
-//appt time was clicked
-$('.list-group').on('click', function () {
-
-    var text = $(this)
-        .text()
-        .trim();
-    var textInput = $('<textarea>').addClass('form-control').val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger('focus');
+hours.forEach((hour, i) => {
+    let rHour = i + 9;
+    document.querySelector('.container').innerHTML += `
+        <div class='row'>
+            <div class='col-md-1 hour'>
+                ${hour}
+            </div>
+            <textarea class='col-md-10 ${rHour < cHour ? "past": rHour > cHour ? "future": "present"}'></textarea>
+            <div class='col-md-1 saveBtn'>
+            <i class="far fa-save"></i>
+            </div>
+        </div>
+    
+    `;
 });
 
-//save button clicked
-$('.save').click(function () {
-    var apptText = $('.task').val();
+const handleClick = ()=> {
+    store = [];
 
-    if (apptText) {
-        createAppt(apptList);
+    for (let i = 0; i < $("textarea").length; i++) {
+        store.push($('textarea').eq(i).val())
+    };
 
-        appt.push({
-            text: apptText
-        });
-        saveAppts();
-    }
-
-})
-
-
-//save appts to local storage
-var saveAppts = function () {
-    localStorage.setItem("appts", JSON.stringify(appt));
+    localStorage.hours = JSON.stringify(store);
 };
+
+document.querySelector('.saveBtn').addEventListener('click',handleClick);
